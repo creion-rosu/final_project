fa47fbf88f5aff56649ff82db617f9b0e28a61ea
creion-rosu <creionrosu@gmail.com>
2025-05-08 04:58:19 -0400

    initial commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    # Define the URL for the emotion predict API
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    # Create the payload with the text to be analyzed
+    myobj = { "raw_document": { "text": text_to_analyse } }
+
+    # Set the headers with the required model ID for the API
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    # Make a POST request to the API with the payload and headers
+    response = requests.post(url, json = myobj, headers = header)
+
+    # Parse the response from the API
+    formatted_response = json.loads(response.text)
+
+    # Save the score of mentioned emotions
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    # Find the name of the emotion with the highest score
+    for key, val in formatted_response['emotionPredictions'][0]['emotion'].items():
+        if val == max(formatted_response['emotionPredictions'][0]['emotion'].values()):
+            dominant_emotion_name = key 
+
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion_name
+        }
